b035b9d9338551dfa51490928180e277b7ac9cae
Bahnas <bahnas114@gmail.com>
2025-10-15 19:20:13 +0300

    Add splash screen assets and implement onboarding screen
    - Added new images for splash screen: pet.png and splash.png.
    - Added SVG icon for splash screen: splash_icon.svg.
    - Updated flutter_native_splash.yaml to include new splash screen assets.
    - Implemented HomeScreen and added routing for home screen.
    - Created app_colors.dart and app_text_styles.dart for consistent theming.
    - Added font_weight_helper.dart for font weight management.
    - Enhanced OnboardingScreen with images, text, and navigation to HomeScreen.
    - Updated pubspec.yaml to include new asset directories.

--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_finder_app/core/helpers/extensions.dart';
+import 'package:pet_finder_app/core/helpers/spacing.dart';
+import 'package:pet_finder_app/core/routing/routes.dart';
+import 'package:pet_finder_app/core/theme/app_colors.dart';
+import 'package:pet_finder_app/core/theme/app_text_styles.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 32.w),
+          child: Column(
+            children: [
+              const Expanded(
+                flex: 2,
+                child: Image(
+                  image: AssetImage('assets/images/pet.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              verticalSpace(32),
+              Text(
+                'Find Your Best\nCompanion With Us',
+                style: AppTextStyles.font32BlackBold,
+                textAlign: TextAlign.center,
+              ),
+              verticalSpace(16),
+              Text(
+                'Join & discover the best suitable pets as\nper your preferences in your location',
+                style: AppTextStyles.font16LightGrayRegular,
+                textAlign: TextAlign.center,
+              ),
+              verticalSpace(32),
+              SizedBox(
+                width: double.infinity,
+                height: 44.h,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.pushReplacementNamed(Routes.homeScreen);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.buttonBackGround,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(26),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.pets, color: Colors.white),
+                      horizontalSpace(8),
+                      Text(
+                        'Get Started',
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              verticalSpace(40),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,5 @@ import 'package:pet_finder_app/pet_finder_app.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
-  
-  runApp( PetFinderApp(appRouter: AppRouter(),));
+  runApp(PetFinderApp(appRouter: AppRouter()));
 }
-
