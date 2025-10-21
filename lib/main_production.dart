b2640b3409049444ca9a8dae68a74877483b5938
Bahnas <bahnas114@gmail.com>
2025-10-21 19:24:41 +0300

    feat: Integrate Firebase for development and production environments

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_finder_app/pet_finder_app.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
+import 'firebase_options_prod.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   setupGetIt();
   runApp(PetFinderApp(appRouter: AppRouter()));
 }
